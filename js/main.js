35c32ddb80f4269708ad25d0cf3f60dd283885c8
Danilazter <89081160+Danilazter@users.noreply.github.com>
2021-08-20 16:51:44 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,16 +3,22 @@ $(document).ready(function () {
     var floorPath = $(".home-image path"); // каждый отдельный этаж в SVG
     var counterUp = $(".counter-up") /* кнопка увеличения этажа*/
     var counterDown = $(".counter-down"); /* кнопка меньшения этажа */
-   
-   
+    var modal = $(".modal");
+    var modaCloseButton = $(".modal-close-button");
+    var viewFlatsButton = $(".view-flats");
+
     // функция при наведении мышью на этаж
     floorPath.on("mouseover", function () {
-         floorPath.removeClass("current-floor"); //удаляем активный класс у этажей
+        floorPath.removeClass("current-floor"); //удаляем активный класс у этажей
         currentFloor = $(this).attr("data-floor"); // получаем значение текущего этажа
         $(".counter").text(currentFloor); // записываем значение этажа в счетчик справа
     });
 
-    counterUp.on("click", function () { // отслеживаем клик по кнопке вверх
+    floorPath.on("click", toggleModal); /* при клике на этаж, вызывает окно */
+    modaCloseButton.on("click", toggleModal); /* клик закрывает окно */
+    viewFlatsButton.on("click", toggleModal);
+// отслеживаем клик по кнопке вверх
+    counterUp.on("click", function () { // 
             // проверяем значение этажа, оно не должно быть больше 18
         if (currentFloor < 18) { 
             currentFloor++; // прибавляем один этаж
@@ -27,11 +33,13 @@ $(document).ready(function () {
         // проверяем значение этажа, оно не должно быть меньше 2
         if (currentFloor > 2) {
             currentFloor--; // убавляем один этаж
-             usCurrentFloor = currentFloor.toLocaleString("en-Us", { minimumIntegerDigits: 2, useGroupping: false }); // форматируем переменную с этажом, чтобы было не 01 а не 1
+            usCurrentFloor = currentFloor.toLocaleString("en-Us", { minimumIntegerDigits: 2, useGroupping: false }); // форматируем переменную с этажом, чтобы было не 01 а не 1
             $(".counter").text(usCurrentFloor); // записываем значение счетчика справа  
-             floorPath.removeClass("current-floor"); //удаляем активный класс у этажей
+            floorPath.removeClass("current-floor"); //удаляем активный класс у этажей
             $(`[data-floor=${usCurrentFloor}]`).toggleClass("current-floor"); // подсвечиваем текущий этаж
-       }
-        
-    })
+        } 
+    });
+    function toggleModal() { // функция октрыть/закрыть окно
+        modal.toggleClass("is-open");
+    }
     });
